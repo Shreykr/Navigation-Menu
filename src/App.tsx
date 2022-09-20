5b174d4b82e0c2c91a078832ef7a73af9b5476ab
Shreyas K R <shreyaskr1022@gmail.com>
2022-09-20 22:18:05 +0530

    correct github corner link

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <GithubCorner
-        href='https://navigation-menu-1.netlify.app/'
+        href='https://github.com/Shreykr/Navigation-Menu'
         bannerColor='#fff'
         octoColor='#000'
         size={80}
